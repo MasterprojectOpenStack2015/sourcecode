34718d652613cdf15d7c677be5889f69d77e6386
mpws04 <mpws04@mpws04-Precision-T3600>
2015-06-05 13:18:50 +0200

    script

--- ansible/install.sh
+++ ansible/install.sh
@@ -2,6 +2,7 @@
 
 #ip of nodes (network must be setup as described here: http://docs.openstack.org/kilo/install-guide/install/apt/content/ch_basic_environment.html):
 USER="vm"
+SUDO_PASS="vm"
 controller_node="$USER@192.168.100.11"
 network_node="$USER@192.168.100.21"
 compute_node1="$USER@192.168.100.31"
@@ -46,9 +47,9 @@ ssh-copy-id -i ~/.ssh/id_rsa.pub $compute_node1
 
 
 
-echo "\n\n[controller]\n\t$controller_node" | sudo tee /etc/ansible/hosts
-echo "\n\n[network]\n\t$network_node" | sudo tee -a /etc/ansible/hosts
-echo "\n\n[compute]\n\t$compute_node1" | sudo tee -a /etc/ansible/hosts
+echo "\n\n[controller]\n\t$controller_node ansible_sudo_pass='$SUDO_PASS'" | sudo tee /etc/ansible/hosts
+echo "\n\n[network]\n\t$network_node ansible_sudo_pass='$SUDO_PASS'" | sudo tee -a /etc/ansible/hosts
+echo "\n\n[compute]\n\t$compute_node1 ansible_sudo_pass='$SUDO_PASS'" | sudo tee -a /etc/ansible/hosts
 
 echo "Testing Network Configuration..."
 #ansible controller -a "ping -c 4 openstack.org"
@@ -59,14 +60,23 @@ echo "Testing Network Configuration..."
 #TODO for real distribution: configure NTP on the nodes
 
 echo "Installing OpenStack..."
-ansible all -b --ask-become-pass -m apt -a 'pkg=ubuntu-cloud-keyring state=installed update_cache=yes'
-ansible all -a "echo \"deb http://ubuntu-cloud.archive.canonical.com/ubuntu\" \"trusty-updates/kilo main\" > /etc/apt/sources.list.d/cloudarchive-kilo.list"
-ansible all -b --ask-become-pass -m apt -a 'update_cache=yes'
-ansible all -b --ask-become-pass -m apt -a 'upgrade=dist'
+ansible all -b -m apt -a 'pkg=ubuntu-cloud-keyring state=installed update_cache=yes'
+ansible all -b -m lineinfile -a "dest=/etc/apt/sources.list.d/cloudarchive-kilo.list create=yes line=\"deb http://ubuntu-cloud.archive.canonical.com/ubuntu trusty-updates/kilo main\""
+ansible all -b -m apt -a 'update_cache=yes'
+ansible all -b -m apt -a 'upgrade=dist'
 echo "Installation Done."
 
 
 echo "Installing MySql..."
-ansible controller -b --ask-become-pass -m apt -a 'pkg=mariadb-server state=installed update_cache=yes'
-ansible controller -b --ask-become-pass -m apt -a 'pkg=python-mysqldb state=installed update_cache=yes'
+ansible controller -b -a "locale-gen de_DE.UTF-8"
+ansible controller -b -a "update-locale LANG=de_DE.UTF-8"
+ansible controller -b -m apt -a 'pkg=mariadb-server state=installed update_cache=yes'
+ansible controller -b -m apt -a 'pkg=python-mysqldb state=installed update_cache=yes'
+
+ansible controller -b -a "rm /etc/mysql/conf.d/mysqld_openstack.cnf"
+ansible controller -b -m lineinfile -a "dest=/etc/mysql/conf.d/mysqld_openstack.cnf create=yes line=\"[mysqld]\nbind-address = 192.168.100.11\n\ndefault-storage-engine = innodb\ninnodb_file_per_table\ncollation-server = utf8_general_ci\ninit-connect = \'SET NAMES utf8\'\ncharacter-set-server = utf8\""
+ansible controller -b -a "service mysql restart"
+ansible controller -b -a "mysql_secure_installation"
 echo "Installation Done."
+
+
