6743b62dc5120597077052fa4d64383c654362a0
mpws02 <mpws02@mpws02>
2015-07-14 15:35:03 +0200

    modified installation of libvirt

--- ansible/installation/install_lib_virt.sh
+++ ansible/installation/install_lib_virt.sh
@@ -8,7 +8,7 @@ then
 	# error: Failed to define domain from /tmp/openstack_installation/images/network/creation.xml
 	# error: unknown OS type hvm
 	sudo apt-get -y remove virtinst libvirt-bin
-	sudo apt-get install qemu-kvm || exit 1
+	sudo apt-get -y install qemu-kvm || exit 1
 fi
 
 # install libvirt
@@ -23,8 +23,8 @@ then
 	# permission denied
 	# http://wiki.libvirt.org/page/Failed_to_connect_to_the_hypervisor#Permission_denied
 	echo "setting permissions for all users to use the \"virsh\" command"
-	sudo sed '/unix_sock_rw_perms\s*=.*/c\unix_sock_rw_perms = "0777" # replaced by install_lib_virt.sh' /etc/libvirt/libvirtd.conf
-	sudo service libvirt-bin start
+	sudo sed -i '/unix_sock_rw_perms\s*=.*/c\unix_sock_rw_perms = "0777" # replaced by install_lib_virt.sh' /etc/libvirt/libvirtd.conf
+	sudo service libvirt-bin restart
 fi
 
 
