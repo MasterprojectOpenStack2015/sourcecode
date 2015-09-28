0abdc7297ffff657c7351532058614101e0e80a3
mpws01 <mpws01@mpws01>
2015-09-28 16:47:40 +0200

    checkin services beforehand

--- experiments/01_control_crash/setup.sh
+++ experiments/01_control_crash/setup.sh
@@ -2,6 +2,18 @@ heading SETUP
 
 source "$variables"
 
+subheading Checking services
+
+
+services="`ssh ubuntu@controller 'source admin-openrc.sh && nova service-list || echo down'`"
+log -vvv "$services"
+if echo "$services" | grep -q down
+then
+  log -v -vv "$services"
+  log -v -vv -vvv "ERROR: An OpenStack service is down. This experiment may fail in unexpected ways. (Press Ctrl+C to exit.)"
+  ask_user_to_step || exit 1
+fi
+
 subheading Creating instance on controller
 
 exitcode=0
