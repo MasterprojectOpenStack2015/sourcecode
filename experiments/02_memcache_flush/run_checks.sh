a2ece26c287a6ad7400b98a800fe0ac57cf1f9d5
mpws01 <mpws01@mpws01>
2015-09-28 18:47:19 +0200

    tst 2 works

--- experiments/02_memcache_flush/run_checks.sh
+++ experiments/02_memcache_flush/run_checks.sh
@@ -3,7 +3,9 @@ source "$variables"
 heading Running checks
 
 subheading attempting to create new token using first token
+
 log -vvv "(full response written to $TMP_PATH/check_curl.log)"
+
 curl -s -S -i \
   -H "Content-Type: application/json" \
   -d '
@@ -18,14 +20,19 @@ curl -s -S -i \
 }' http://controller:5000/v3/auth/tokens > "$TMP_PATH/check_curl.log"
 
 cat $TMP_PATH/check_curl.log | log -i -vvv
+log -vvv
+
+subheading token
 
 if grep -q "HTTP/1.1 201 Created" "$TMP_PATH/check_curl.log"
 then
-    log -vv -vvv -n "Token created: "
-    grep "X-Subject-Token:" "$TMP_PATH/check_curl.log" | log -i -vv -vvv
+  log_ok -v "Token created."
+  log_ok -vv -vvv -n "Token created: "
+  grep "X-Subject-Token:" "$TMP_PATH/check_curl.log" | log -i -v -vv -vvv
 else
-    log -vv -vvv -n "ERROR:"
-    tail -n1 "$TMP_PATH/check_curl.log" | log -i -vv -vvv
+  log_error -v "No token created."
+  log_error -vv -vvv -n "No token created: "
+  tail -n1 "$TMP_PATH/check_curl.log" | log -i -vv -vvv
 fi
 
-log -vvv
+log -vvv -vv
