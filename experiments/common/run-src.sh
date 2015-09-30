5d41cc39ac66b1ed3a4259c470448aa1158b1cbe
mpws01 <mpws01@mpws01>
2015-09-30 14:54:07 +0200

    added tools path of installation

--- experiments/common/run-src.sh
+++ experiments/common/run-src.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PATH=`dirname "$0"`/../common:`dirname "$0"`/../../installaion/tools:$PATH
+export PATH=`dirname "$0"`/../common:`dirname "$0"`/../../installation/tools:$PATH
 export logging_level=${1:1}
 experiment_folder=`pwd`
 experiment_folder=`filename "$experiment_folder"`
