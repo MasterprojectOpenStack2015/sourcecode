e1f0a2007448d29e2fc5c1bfd1538344c899e736
mpws01 <mpws01@mpws01>
2015-09-30 15:10:47 +0200

    waiting for controller to shut down works

--- experiments/05_control_crash/break.sh
+++ experiments/05_control_crash/break.sh
@@ -5,12 +5,12 @@ heading CREATING FAULT
 subheading shutting the controller down
 virsh shutdown controller | log -vvv --input 
 
-subheading "Waiting for the controller to disappear."
+subheading "Waiting for the controller to shut down."
 
 
-list_domains | log -vvv --input "active domains:"
+list_domains --active | log -vvv --input "Active domains:"
 
-while list_domains | grep -q controller
+while list_domains --active | grep -q controller
 do
   sleep 1
   log -vvv .
