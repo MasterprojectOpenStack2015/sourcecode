d462de5fd80bd5e7192ef3bafa2aa0046073e00b
mpws01 <mpws01@mpws01>
2015-09-30 14:53:46 +0200

    experiment 1 waiting for controller to shut down

--- experiments/01_control_crash/break.sh
+++ experiments/01_control_crash/break.sh
@@ -1,5 +1,12 @@
 heading CREATING FAULT
 
 subheading killing the controller
-virsh shutdown controller | log -vvv --input 
+virsh shutdown controller 2>&1 | log -vvv --input 
 
+subheading waiting for the controller to shut down
+
+while list_domains | grep -q controller
+do
+	sleep 1
+	log -vvv -n .
+done
