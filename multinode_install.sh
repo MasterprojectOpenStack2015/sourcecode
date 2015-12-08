7b495bb9e2a76f00e08700ec8c8bffde5bd6bad8
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-12-08 14:01:38 +0100

    fix installation of libvirt plugin

--- multinode_install.sh
+++ multinode_install.sh
@@ -1,4 +1,7 @@
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get -y update
 sudo apt-get -y install software-properties-common ansible
-ansible-playbook multinode_installation/playbooks/install_packages_local.yml --ask-sudo-pass || exit 1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ansible-playbook $DIR/multinode_installation/playbooks/install_packages_local.yml --ask-sudo-pass || exit 1
+cd $DIR/multinode_installation
+vagrant plugin install vagrant-libvirt || exit 1
