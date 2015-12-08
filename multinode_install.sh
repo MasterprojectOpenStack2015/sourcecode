1d8abe62e75d2e10f4ce487ceef5f7a5b3d5109c
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-12-08 14:59:37 +0100

    missing sudo

--- multinode_install.sh
+++ multinode_install.sh
@@ -4,4 +4,4 @@ sudo apt-get -y install software-properties-common ansible
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ansible-playbook $DIR/multinode_installation/playbooks/install_packages_local.yml --ask-sudo-pass || exit 1
 cd $DIR/multinode_installation
-vagrant plugin install vagrant-libvirt || exit 1
+sudo vagrant plugin install vagrant-libvirt || exit 1
