f7ca2a5406509c0d63f647cfe2d33f713507dcbd
Marvin Keller <marvin.keller@student.hpi.de>
2015-12-15 14:59:48 +0100

    software-properties-common not being installed before adding PPA added fixed IPs for datacenters

--- multinode_install.sh
+++ multinode_install.sh
@@ -1,6 +1,8 @@
+sudo apt-get update
+sudo apt-get install software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get -y update
-sudo apt-get -y install software-properties-common ansible
+sudo apt-get update
+sudo apt-get install -y ansible
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ansible-playbook $DIR/multinode_installation/playbooks/install_packages_local.yml --ask-sudo-pass || exit 1
 cd $DIR/multinode_installation
